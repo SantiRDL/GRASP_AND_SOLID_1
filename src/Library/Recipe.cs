369642df3c828a4f264b84840ea5254eac9024e6
estebancasalas <e.casalas@outlook.com>
2021-09-05 16:56:01 -0300

    hola

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -33,6 +33,23 @@ namespace Full_GRASP_And_SOLID.Library
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+            Console.WriteLine($"El costo total de la receta es:{this.GetProductionCost()}");
+        }
+        /* 
+        Decidimos asignar esta responsabilidad a la clase Recipe basándonos en el patrón Expert. Para este metodo se
+        necesita información sobre: los pasos, la cantidad de productos, el costo por unidad, el equipamiento, el costo 
+        por hora del equipamiento y el timepo de uso. La única clase que tiene información sobre todo eso es Recipe. 
+        */
+        public double GetProductionCost()
+        {
+            double CostoInsumos = 0;
+            double CostoEquipamiento = 0;
+            foreach (Step step in this.steps)
+            {
+                CostoInsumos += step.Input.UnitCost * step.Quantity;
+                CostoEquipamiento += step.Equipment.HourlyCost * Convert.ToDouble(step.Time);
+            }
+            return CostoEquipamiento + CostoInsumos;
         }
     }
 }
